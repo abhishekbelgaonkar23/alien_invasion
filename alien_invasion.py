66903d62e4aeb686261872fa6db7194f78981ab1
abhishekbelgaonkar23 <abhishekbelgaonkar23@gmail.com>
2024-09-10 21:49:53 +0530

    added settings.py file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,23 @@
 import sys
 import pygame
+from settings import settings
 
-class AlienInvasion: # overall class to manage game assets and behavior
-	def __init__(self): 
-		pygame.init()
-		self.screen = pygame.display.set_mode(1200, 800)
-		pygame.display.set_caption("Alien Invasion")
-		
-	def run_game(self):
-		while True: # main loop for the game
-			for event.type in pygame.event.get(): # watch for keyboard and mouse events
-				if event.type == pygame.QUIT:
-					sys.exit()
-			pygame.display.flip() # make the most recently drawn screen visible
-if __name__ == '__main__': # make a game instance, and run the game
-	
+class AlienInvasion:  # class to manage game assets and behavior
+    def __init__(self): 
+        pygame.init()
+        self.settings = settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+    
+    def run_game(self):
+        while True:  # main loop
+            for event in pygame.event.get():  # watch for keyboard & mouse events
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()  # make the most recently drawn screen visible
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
+
